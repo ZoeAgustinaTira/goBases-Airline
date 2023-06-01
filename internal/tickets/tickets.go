ec5a97661981f59ed45a1ab9a89d8bfaab37029f
Gregorio Garcia <gregorio.garcia@externos.bci.cl>
2023-06-01 00:00:12 -0300

    add GetCountByPeriod method without ch

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,5 +1,12 @@
 package tickets
 
+import (
+	"fmt"
+	"github.com/ZoeAgustinaTira/goBases-Airline/internal/tickets/repository"
+	"strconv"
+	"strings"
+)
+
 /*
 // ejemplo 1
 func GetTotalTickets(destination string) (int, error) {
@@ -13,3 +20,35 @@ func GetMornings(time string) (int, error) {}
 // ejemplo 3
 func AverageDestination(destination string, total int) (int, error) {}
 */
+func GetCountByPeriod(periodo string) {
+	var inicio, fin int
+	switch periodo {
+	case "madrugada":
+		inicio, fin = 0, 6
+	case "mañana":
+		inicio, fin = 7, 12
+	case "tarde":
+		inicio, fin = 13, 19
+	case "noche":
+		inicio, fin = 20, 23
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Error durante el procesamiento:", err)
+		}
+	}()
+
+	contar := 0
+	for _, ticket := range repository.TicketList {
+		hora, err := strconv.Atoi(strings.Split(ticket.Time, ":")[0])
+		if err != nil {
+			panic("No se pudo dividir el string de la hora")
+		}
+		if hora >= inicio && hora <= fin {
+			contar++
+		}
+	}
+
+	fmt.Printf("Hay %d pasajeros volando en el periodo '%s'", contar, periodo)
+}
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/ZoeAgustinaTira/goBases-Airline/internal/tickets"
 	"github.com/ZoeAgustinaTira/goBases-Airline/reader"
 )
 
 func main() {
 	reader.ReadFile()
 	//total, err := tickets.GetTotalTickets("Brazil")
+
+	tickets.GetCountByPeriod("noche")
+
 }
