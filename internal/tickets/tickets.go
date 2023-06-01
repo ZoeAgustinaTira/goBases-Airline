2ac7e6d787a9ce52014b4100d33760ec77f1038e
Zoe Agustina Tira <zoeagustinatira@gmail.com>
2023-06-01 15:48:48 -0300

    add avgByDestination

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,15 +1,29 @@
 package tickets
 
+import (
+	"github.com/ZoeAgustinaTira/goBases-Airline/domain"
+	"github.com/ZoeAgustinaTira/goBases-Airline/internal/tickets/repository"
+)
+
 /*
 // ejemplo 1
 func GetTotalTickets(destination string) (int, error) {
 
-	return 0,nil
-}
+		return 0,nil
+	}
 
 // ejemplo 2
 func GetMornings(time string) (int, error) {}
-
-// ejemplo 3
-func AverageDestination(destination string, total int) (int, error) {}
 */
+func AverageDestination(destination string) (float64, error) {
+	var ticketbc []domain.Ticket
+
+	for _, ticket := range repository.TicketList {
+		if ticket.Country == destination {
+			ticketbc = append(ticketbc, ticket)
+		}
+	}
+	avg := float64(len(ticketbc)) * 100.0 / float64(len(repository.TicketList))
+
+	return avg, nil
+}
