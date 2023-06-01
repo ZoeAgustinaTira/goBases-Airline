be807daddb90a9030bef42797ecce0f6ac1cfce6
Zoe Agustina Tira <zoeagustinatira@gmail.com>
2023-06-01 15:32:24 -0300

    err

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/ZoeAgustinaTira/goBases-Airline/internal/tickets"
+	"github.com/ZoeAgustinaTira/goBases-Airline/reader"
 )
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	reader.ReadFile()
+	//total, err := tickets.GetTotalTickets("Brazil")
 }
