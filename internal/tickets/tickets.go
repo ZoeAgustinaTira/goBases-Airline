045cd2fecc4020e7c950d394443cafa8a7c57ca3
Zoe Agustina Tira <zoeagustinatira@gmail.com>
2023-05-31 23:03:48 -0300

    add return

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -4,7 +4,9 @@ type Ticket struct {
 }
 
 // ejemplo 1
-func GetTotalTickets(destination string) (int, error) {}
+func GetTotalTickets(destination string) (int, error) {
+	return 0, nil
+}
 
 // ejemplo 2
 func GetMornings(time string) (int, error) {}
